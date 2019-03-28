d2ba98f3577f45918b2eb64b7fdae318016984d2
LAPTOP-T14F33K9\dylamug <dylamug@gmail.com>
2019-03-28 23:06:33 +0200

    321

---
+++ SP_yair/Assets/Satan/SatanScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatanScript : MonoBehaviour
+{
+
+    public float timeStamp;
+    bool hasStomped = false;
+   public bool triggered = false;
+    public MedKitSpawner mySpawner;
+    
+    Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggered = false;
+        myAnim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (triggered == true)
+        {
+            if (hasStomped == false)
+            {
+                timeStamp = Time.time + 5;
+                hasStomped = true;
+
+            }
+
+
+
+
+
+            if (Time.time >= timeStamp)
+            {
+                myAnim.SetTrigger("Stomp");
+                hasStomped = false;
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            triggered = true;
+        }
+    }
+
+    void Spawnz()
+    {
+        mySpawner.Spawn();
+    }
+
+
+
+}
